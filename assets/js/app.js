faf1bbcde5763277b73a0f007cb6a24b1dcab1c9
David Simmons <shortlyportly@googlemail.com>
2021-01-20 20:08:05 +0000

    formatted modal and forms

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,23 +16,18 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
-import 'alpinejs'
+import Alpine from 'alpinejs'
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
 // Update liveSocket declaration to work with Alpine.js
-let liveSocket = new LiveSocket('/live', Socket, {
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {_csrf_token: csrfToken},
   dom: {
-    onBeforeElUpdated(from, to) {
-      if (from.__x) {
-        window.Alpine.clone(from.__x, to)
-      }
-    }
-  },
-  params: {
-    _csrf_token: csrfToken
-  },
-  hooks: Hooks
+   onBeforeElUpdated(from, to){
+     if(from.__x){ Alpine.clone(from.__x, to) }
+   }
+ }
 })
 
 // Show progress bar on live navigation and form submits
